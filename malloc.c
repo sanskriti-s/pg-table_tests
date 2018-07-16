0bb7ae90e64335267dbd52967dd005e8a825328b
Joe Lawrence <joe.lawrence@redhat.com>
2018-07-16 17:43:49 -0400

    malloc: add test to run-tests

--- malloc.c
+++ malloc.c
@@ -2,14 +2,9 @@
 #include <stdlib.h>
 
 #define SIZE_GiB		(1ULL << 30)
-#define SIZE_TiB		(1ULL << 40)
-
-/* 2 ^ 47 = 256 TiB, split in half for user/kernel address space. */
-#define MAX_USER_VM_4LVL	(128ULL * SIZE_TiB)
 
 int main()
 {
-	int ret;
 	int i=0;
 	void *ptr;
 
@@ -23,17 +18,5 @@ int main()
 	
 	printf("%d GiB allocated.\n",i);
 
-	if ((i * SIZE_GiB) > MAX_USER_VM_4LVL)
-	{
-		fprintf(stderr,
-			"Unexpected malloc total (%llu bytes > %llu bytes)\n",
-			i * SIZE_GiB, MAX_USER_VM_4LVL);
-		ret = EXIT_FAILURE;
-	}
-	else
-	{
-		ret = EXIT_SUCCESS;
-	}
-
-	return ret;
+	return 0;
 }
