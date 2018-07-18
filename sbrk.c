1d3471956744658d8a077a659e4f962d0936f438
Sanskriti Sharma <sansharm@redhat.com>
2018-07-18 10:19:09 -0400

    Changed sbrk and run-tests

--- sbrk.c
+++ sbrk.c
@@ -2,15 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define SIZE_GiB		(1ULL << 30)
-#define SIZE_TiB		(1ULL << 40)
-
-/* 2 ^ 47 = 256 TiB, split in half for user/kernel address space. */
-#define MAX_USER_VM_4LVL	(128ULL * SIZE_TiB)
+#define SIZE_GiB	(1ULL << 30)
 
 int main()
 {
-	int ret;
 	int i=0;
 	void *ptr;
 
@@ -23,18 +18,5 @@ int main()
 	}
 	
 	printf("%d GiB allocated.\n",i);
-
-	if ((i * SIZE_GiB) > MAX_USER_VM_4LVL)
-	{
-		fprintf(stderr,
-			"Unexpected sbrk total (%llu bytes > %llu bytes)\n",
-			i * SIZE_GiB, MAX_USER_VM_4LVL);
-		ret = EXIT_FAILURE;
-	}
-	else
-	{
-		ret = EXIT_SUCCESS;
-	}
-
-	return ret;
+	return 0;
 }
